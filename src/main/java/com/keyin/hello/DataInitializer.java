182db1f59cdd2eb4561f7419025ad083b2edf001
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-07-18 20:44:40 -0230

    Create DataInitializer file

---
+++ src/main/java/com/keyin/hello/DataInitializer.java
@@ -0,0 +1,41 @@
+//DataInitializer
+package com.keyin.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @Autowired
+    private LanguageRepository languageRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<String> languages = Arrays.asList("French", "Japanese", "Arabic", "Spanish");
+
+        for (String lang : languages) {
+            if (languageRepository.findByName(lang) == null) {
+                languageRepository.save(new Language(lang));
+            }
+        }
+
+        Greeting greeting = new Greeting();
+        greeting.setName("Sample Greeting");
+        greeting.setGreeting("Hello in different languages");
+
+        for (String lang : languages) {
+            Language language = languageRepository.findByName(lang);
+            greeting.getLanguages().add(language);
+        }
+
+        greetingService.createGreeting(greeting);
+    }
+}
